fd31cd98e8b1c26d18e1642870fdc09ae765e5b9
Ryan Ribeiro <rmr@ryanribeiro.com>
2019-02-04 22:57:25 -0500

    Setting up unit testing

---
+++ src/test/java/AddressBookTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+public class AddressBookTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void testGetBuddiesList() {
+    }
+
+    @org.junit.Test
+    public void testAddAndRemoveBuddy() {
+    }
+
+    @org.junit.Test
+    public void testRemoveAllBuddies() {
+    }
+
+    @org.junit.Test
+    public void testPrintAddressBook() {
+    }
+}
---
+++ src/test/java/BuddyInfoTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BuddyInfoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testGetAndSetName() {
+    }
+
+    @Test
+    public void testGetAndSetAddress() {
+    }
+
+    @Test
+    public void testGetAndSetPhoneNumber() {
+    }
+
+    @Test
+    public void testIsEqual() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}

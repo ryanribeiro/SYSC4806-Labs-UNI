bec273b0331989753beda088b767800fbc7ae4c0
Ryan Ribeiro <rmr@ryanribeiro.com>
2019-02-04 23:17:14 -0500

    Implemented unit testing.

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -2,23 +2,40 @@ import static org.junit.Assert.*;
 
 public class AddressBookTest {
 
+    private AddressBook addressBook;
+    private BuddyInfo buddy1, buddy2, buddy3;
+
     @org.junit.Before
     public void setUp() throws Exception {
-    }
-
-    @org.junit.Test
-    public void testGetBuddiesList() {
+        this.addressBook = new AddressBook();
+        this.buddy1 = new BuddyInfo("Ryan", "Klondike", "555-555-5555");
+        this.buddy2 = new BuddyInfo("John", "Jail", "123-456-7890");
+        this.buddy3 = new BuddyInfo("Jane", "Main Street", "1-800-555-5555");
     }
 
     @org.junit.Test
     public void testAddAndRemoveBuddy() {
+        this.addressBook.addBuddy(this.buddy1);
+        this.addressBook.addBuddy(this.buddy2);
+        this.addressBook.addBuddy(this.buddy3);
+        int size = this.addressBook.getBuddiesList().size();
+
+        assertEquals("Size should be three.", 3, size);
+
+        this.addressBook.removeBuddy(1);
+        size = this.addressBook.getBuddiesList().size();
+
+        assertEquals("Size should now be two.", 2, size);
     }
 
     @org.junit.Test
     public void testRemoveAllBuddies() {
-    }
+        this.addressBook.addBuddy(this.buddy1);
+        this.addressBook.addBuddy(this.buddy2);
+        this.addressBook.addBuddy(this.buddy3);
+        this.addressBook.removeAllBuddies();
+        int size = this.addressBook.getBuddiesList().size();
 
-    @org.junit.Test
-    public void testPrintAddressBook() {
+        assertEquals("All buddies removed, should be empty.", 0, size);
     }
 }
--- src/test/java/BuddyInfoTest.java
+++ src/test/java/BuddyInfoTest.java
@@ -5,27 +5,48 @@ import static org.junit.Assert.*;
 
 public class BuddyInfoTest {
 
+    private BuddyInfo buddy;
+
     @Before
     public void setUp() throws Exception {
+        buddy = new BuddyInfo("Ryan", "Klondike", "555-555-5555");
     }
 
     @Test
     public void testGetAndSetName() {
+        this.buddy.setName("FAKE");
+
+        assertEquals("Name should be 'FAKE'", "FAKE", buddy.getName());
     }
 
     @Test
     public void testGetAndSetAddress() {
+        this.buddy.setAddress("FAKE");
+
+        assertEquals("Address should be 'FAKE'", "FAKE", buddy.getAddress());
     }
 
     @Test
     public void testGetAndSetPhoneNumber() {
+        this.buddy.setPhoneNumber("FAKE");
+
+        assertEquals("Phonenumber should be 'FAKE'", "FAKE", buddy.getPhoneNumber());
     }
 
     @Test
     public void testIsEqual() {
+        BuddyInfo fakeBuddy = new BuddyInfo("FAKE", "Klondike", "555-555-5555");
+        BuddyInfo goodBuddy = new BuddyInfo("Ryan", "Klondike", "555-555-5555");
+
+        assertEquals("Buddies are not the same, should be false", false, this.buddy.isEqual(fakeBuddy));
+        assertEquals("Buddies are the same, should be true", true, this.buddy.isEqual(goodBuddy));
     }
 
     @Test
     public void testToString() {
+        String printout = this.buddy.toString();
+        String expected = "Name: Ryan Address: Klondike Phonenumber: 555-555-5555";
+
+        assertEquals("Strings should match", expected, printout);
     }
 }
